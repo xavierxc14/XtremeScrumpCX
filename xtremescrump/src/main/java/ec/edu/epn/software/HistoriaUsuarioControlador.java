44caaeb3d59aa0b0a080eb0486055fba0483f84e
atikasoft <atikasoft@atikasoft>
2015-01-22 13:05:02 -0500

    listo web

---
+++ xtremescrump/src/main/java/ec/edu/epn/software/HistoriaUsuarioControlador.java
@@ -0,0 +1,31 @@
+package ec.edu.epn.software;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Xavier Naunay <xavier.naunay@markasoft.ec>
+ */
+@ManagedBean(name = "historiaUsuarioBean")
+@SessionScoped
+public class HistoriaUsuarioControlador implements Serializable {
+
+    private String saludo;
+
+    /**
+     * @return the saludo
+     */
+    public String getSaludo() {
+        return saludo;
+    }
+
+    /**
+     * @param saludo the saludo to set
+     */
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+
+}
